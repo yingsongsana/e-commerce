7f56e26dbb3d1ed3150c4a9bf6ea442065d75f9e
yingsongsana <yingsongsana@gmail.com>
2021-04-26 14:09:15 -0400

    Implement data normalization, data in array --> object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,14 +2,6 @@ import memoize from 'lodash.memoize';
 
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -23,3 +15,4 @@ export const selectCollection = memoize(collectionUrlParam =>
     collections => collections[collectionUrlParam]
   )
 );
+
