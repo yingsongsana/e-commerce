e01e9d658e1f49fea0788583a07fcb4d033b8128
yingsongsana <yingsongsana@gmail.com>
2021-04-29 11:44:43 -0400

    Add link routing to collections preview page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component'
 
@@ -6,7 +7,7 @@ import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <Link className='title' to={`/shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
