7f22466c4a5ce913c35b15e4d777b26f2b4537df
yingsongsana <yingsongsana@gmail.com>
2021-05-11 12:52:01 -0400

    Create WithSpinner HOC to handle asynchronous query into backend

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,12 +6,19 @@ import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/fireb
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapShot = null;
 
   componentDidMount() {
@@ -21,15 +28,17 @@ class ShopPage extends React.Component {
     this.unsubscribeFromSnapShot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false })
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
         <div className='shop-page'>
-          <Route exact path={`${match.path}`} component={CollectionsOverview} /> 
-          <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+          <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} /> 
+          <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={loading} {...props} /> } />
         </div>
     )  
   }
